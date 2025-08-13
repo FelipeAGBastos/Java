8e762bde4960281920ffc619fa455b5ba52996ab
Felipe Alves Gomes Bastos <felipeagbastos1@gmail.com>
2025-08-13 11:08:55 -0300

    Update AjustadorAutomaticoDeFuncionarios.java
    Atualização do código.

--- AjustadorAutomaticoDeFuncionarios.java
+++ AjustadorAutomaticoDeFuncionarios.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JAvava {
+public class AjustadorAutomaticoDeFuncionarios {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
@@ -42,3 +42,4 @@ public class JAvava {
         }
     }
 }
+
